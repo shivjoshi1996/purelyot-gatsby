fe10f537457b62edd90e3afab6005ab0dccb0721
shivjoshi1996 <joshi.k.shiv@gmail.com>
2021-09-04 18:57:59 -0700

    WIP navigation, will need to refactor to set max-width without background color breaking

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,22 +2,50 @@ import * as React from 'react';
 import styled from 'styled-components';
 
 const StyledNavigation = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
   font-family: 'Montserrat', sans-serif;
+  width: 100vw;
+  background-color: #fec5bb;
+  min-height: 10vh;
+  padding: 2rem;
 `;
 
 const StyledLogo = styled.a`
   font-family: 'Recoleta', sans-serif;
+  text-decoration: none;
+  color: #2f2e41;
+  font-size: 1.5rem;
+  font-weight: 600;
+`;
+
+const StyledNavigationLinks = styled.ul`
+  display: flex;
+  list-style: none;
+  gap: 1.5rem;
+
+  a {
+    text-decoration: none;
+    color: #2f2e41;
+  }
 `;
 
 function Navigation() {
   return (
     <StyledNavigation>
       <StyledLogo href="/">Purely OT</StyledLogo>
-      <ul>
-        <li>Services</li>
-        <li>Who We are</li>
-        <li>Contact us</li>
-      </ul>
+      <StyledNavigationLinks>
+        <li>
+          <a href="/">Services</a>
+        </li>
+        <li>
+          <a href="/">Who We are</a>
+        </li>
+        <li>
+          <a href="/">Contact us</a>
+        </li>
+      </StyledNavigationLinks>
     </StyledNavigation>
   );
 }
--- src/styles/Typography.js
+++ src/styles/Typography.js
@@ -3,10 +3,16 @@ import { createGlobalStyle } from 'styled-components';
 import font from '../fonts/recoleta-regular.woff2';
 
 const Typography = createGlobalStyle`
+  $primary-colour: #FEC5BB;
+  $secondary-colour: #FFFFFF;
+  $third-colour: #FAE1DD;
+  $main-font-colour: #2F2E41;
+
   @font-face {
     font-family: Recoleta;
     src: url(${font});
   }
+  
   html {
     font-family: 'Montserrat', san-serif;
   }
