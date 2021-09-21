fa0afc0f9f06952f7ab0956d4816ef403511294b
shivjoshi1996 <joshi.k.shiv@gmail.com>
2021-09-21 13:17:06 -0700

    Setting up success page, connected sanity, now need to style

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,5 +1,30 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
-export default function SuccessPage() {
-  return <h1>Thanks for your submission</h1>;
+export default function SuccessPage({ data }) {
+  const { sanitySuccess } = data;
+  console.log(sanitySuccess);
+  return (
+    <>
+      <img src={sanitySuccess.successImage.asset.url} />
+      <h1>{sanitySuccess.heroHeading}</h1>
+      <p>{sanitySuccess.heroDescription}</p>
+      <a href="">{sanitySuccess.heroButtonText}</a>
+    </>
+  );
 }
+
+export const query = graphql`
+  query {
+    sanitySuccess {
+      heroButtonText
+      heroDescription
+      heroHeading
+      successImage {
+        asset {
+          url
+        }
+      }
+    }
+  }
+`;
